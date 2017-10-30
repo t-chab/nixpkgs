92382e8ae8c1f4a02143274a252705c2c365ce03
Domen Kožar <domen@dev.si>
2017-10-30 12:45:36 +0000

    osx_private_sdk: reduce output size and remove it from runtime closures

--- pkgs/os-specific/darwin/apple-source-releases/libsecurity_utilities/default.nix
+++ pkgs/os-specific/darwin/apple-source-releases/libsecurity_utilities/default.nix
@@ -18,8 +18,8 @@ appleDerivation {
       '<IOKit/pwr_mgt/IOPMLibPrivate.h>' \
       '"${IOKit}/Library/Frameworks/IOKit.framework/Headers/pwr_mgt/IOPMLibPrivate.h"'
 
-    cp ${osx_private_sdk}/PrivateSDK10.9.sparse.sdk/usr/include/security_utilities/utilities_dtrace.h lib
-    cp -R ${osx_private_sdk}/PrivateSDK10.9.sparse.sdk/usr/local/include/bsm lib
+    substituteInPlace lib/dtrace.mk --replace "/usr/sbin/dtrace" "${dtrace}/bin/dtrace"
+    cp -R ${osx_private_sdk}/include/bsm lib
   '' + stdenv.lib.optionalString (!stdenv.cc.nativeLibc) ''
     substituteInPlace lib/vproc++.cpp --replace /usr/local/include/vproc_priv.h ${stdenv.libc}/include/vproc_priv.h
   '';
--- pkgs/os-specific/darwin/osx-private-sdk/default.nix
+++ pkgs/os-specific/darwin/osx-private-sdk/default.nix
@@ -1,8 +1,23 @@
 { stdenv, fetchFromGitHub }:
 
-fetchFromGitHub {
-  owner = "samdmarshall";
-  repo = "OSXPrivateSDK";
-  rev = "f4d52b60e86b496abfaffa119a7d299562d99783";
-  sha256 = "0bv0884yxpvk2ishxj8gdy1w6wb0gwfq55q5qjp0s8z0z7f63zqh";
+stdenv.mkDerivation {
+  name = "OSXPrivateSDK";
+
+  src = fetchFromGitHub {
+    owner = "samdmarshall";
+    repo = "OSXPrivateSDK";
+    rev = "f4d52b60e86b496abfaffa119a7d299562d99783";
+    sha256 = "0bv0884yxpvk2ishxj8gdy1w6wb0gwfq55q5qjp0s8z0z7f63zqh";
+  };
+
+  # NOTE: we install only headers that are really needed to keep closure sie
+  # reasonable.
+  installPhase = ''
+    mkdir -p $out/include
+    cp PrivateSDK10.10.sparse.sdk/usr/local/include/sandbox/private.h $out/include/sandbox_private.h
+    cp -R PrivateSDK10.10.sparse.sdk/System/Library/Frameworks/Security.framework/Versions/A/PrivateHeaders $out/include/SecurityPrivateHeaders
+    cp -R PrivateSDK10.10.sparse.sdk/System/Library/Frameworks/CoreFoundation.framework/Headers $out/include/CoreFoundationPrivateHeaders
+    cp -R PrivateSDK10.10.sparse.sdk/usr/include/xpc $out/include/
+    cp -R PrivateSDK10.10.sparse.sdk/usr/local/include/bsm $out/include/bsm
+  '';
 }
